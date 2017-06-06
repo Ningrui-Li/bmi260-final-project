ba4687c783c294bfd22e2714fb62c3ac4e2fcd80
Ningrui Li <ningrui.li@stanford.edu>
2017-06-06 11:51:11 -0700

    Started function for extracting features from the DCE image. Currently function can read in each patient's DCE image volume and cast it to a numpy array.

--- extract_features.py
+++ extract_features.py
@@ -2,19 +2,37 @@ from os import listdir
 from os.path import join
 
 import dicom
+import SimpleITK as sitk
 
-def extract_metadata_features(patient_mri_dir):
+def extract_metadata_features(mri_dir):
     '''
     Given a directory with MR image slices (DICOM), look at the first DICOM
     file and extract various metadata features from the patient, like his age.
     '''
-    dcm_names = listdir(patient_mri_dir)
+    dcm_names = listdir(mri_dir)
     num_images = len(dcm_names)
 
     # Read in first image to get image size info.
-    dcm = dicom.read_file(join(patient_mri_dir, dcm_names[0]))
+    dcm = dicom.read_file(join(mri_dir, dcm_names[0]))
 
     # Add patient's age, size, and weight.
     features = [int(dcm.PatientAge[:-1]), int(dcm.PatientWeight)]
 
     return features
+
+
+def extract_dce_features(dce_dir):
+    '''
+    Extract features from the provided DCE image.
+    '''
+    # Get header and image volume file names.
+    for f in listdir(dce_dir):
+        if f.endswith('mhd'):
+            header_file = f
+        else:
+            img_file = f
+
+    img_itk = sitk.ReadImage(join(dce_dir, header_file))
+    img = sitk.GetArrayFromImage(img_itk)
+    print(img.shape)
+    return
